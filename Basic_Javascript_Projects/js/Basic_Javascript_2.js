25ccffcf0bb7658865a1c0a12575249f58cf9043
Chromatonics <61987650+Chromatonics@users.noreply.github.com>
2020-03-30 13:01:21 -0600

    Add files via upload

---
+++ Basic_Javascript_Projects/js/Basic_Javascript_2.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var str= "Blah";
+    var result= str.fontcolor("blue")
+    document.getElementById("demo").innerHTML = result;
+  }
+
+  function myFunction_2() {
+      var sentence="I am learning";
+      sentence +=" a lot from this book.";
+      document.getElementById("concatenate").innerHTML=sentence;
+  }
