9e375b7d3415e09c955f75f6b7f3a9d726ca9e3a
Chromatonics <61987650+Chromatonics@users.noreply.github.com>
2020-03-30 12:02:21 -0600

    Add files via upload

---
+++ Basic_Javascript_Projects/js/Basic_Javascript_1.js
@@ -0,0 +1,16 @@
+document.write("I said, \"Hello World!\"");
+
+var B=("sea"+"horse");
+
+document.write(B)
+
+var cats= "The Purr Squad", Black="Flufferton", Orange="Karl", Gray="Cat Face";
+var Orange=Orange.fontcolor("orange");
+
+document.write(Orange);
+
+document.write(3+3);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
